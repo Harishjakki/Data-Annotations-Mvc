56df3e98d167d59e5737cc8d39a2b833ec2bf9ae
Harishjakki <78526836+Harishjakki@users.noreply.github.com>
2021-03-17 11:24:11 +0530

    Add files via upload

---
+++ Employee.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace WebApplication20.Models
+{
+    public class Employee
+    {
+        [Required (ErrorMessage = "Employee Id must")]
+        [Display (Name = "Employee Id")]
+        public int EmpId { get; set; }
+        [Required(ErrorMessage = "Employee Name must")]
+        [Display(Name = "Employee Name")]
+        [MinLength(5,ErrorMessage ="Employee name must and should atleast 5 charecters")]
+        [MaxLength(15, ErrorMessage = "Employee name cant be more than 15 charecters")]
+        public string EmpName { get; set; }
+        [Required(ErrorMessage = "Employee Salary must")]
+        [Display(Name = "Employee Salary")]
+        public int EmpSal { get; set; }
+
+    }
+}
